bdbe72d6c2e092a8d5c1aeac78e684eafae36458
mariokart761 <mariokart761@gmail.com>
2024-10-29 21:34:13 +0800

    fix: 修正為迭代所有年份考古題

--- file_iterator.py
+++ file_iterator.py
@@ -40,38 +40,40 @@ def classify_files(folder_path):
         print(f"發生錯誤: {e}")
         return {}    
 if __name__ == "__main__":
-    考試年份 = 101
-    lv1_folder = f'./{考試年份}年考選部考古題'  #考試年份
-    lv2_folder = list_directories(lv1_folder)  #考試名稱:list
-    data=[]
-    for 考試名稱 in tqdm(lv2_folder, desc='Parsing PDF content', unit="its"):
-        lv3_folder = list_directories(f"{lv1_folder}/{考試名稱}") #考試科目:list
-        for 考試科目 in lv3_folder:
-            lv3_path = f'{lv1_folder}/{考試名稱}/{考試科目}'
-            pdf_file = classify_files(lv3_path)
-            
-            試題_path = f'{lv3_path}/{pdf_file["試題"][0]}'
-            答案_path = f'{lv3_path}/{pdf_file["答案"][0]}'
-            # 檢查是否有更正答案，有則使用"更正答案"，反之則用"答案"
-            更正答案_path = [""]
-            if pdf_file["更正答案"]!=[]:
-                更正答案_path = f'{lv3_path}/{pdf_file["更正答案"][0]}'
-                ans = PDFAnswerExtractor(更正答案_path).get_results()
-            else: ans = PDFAnswerExtractor(答案_path).get_results()
+    exam_years = [101,102,103,104,105,106,107,108,109,110,111,112]
+    for exam_year in exam_years:
+        考試年份 = exam_year
+        lv1_folder = f'./{考試年份}年考選部考古題'  #考試年份
+        lv2_folder = list_directories(lv1_folder)  #考試名稱:list
+        data=[]
+        for 考試名稱 in tqdm(lv2_folder, desc='Parsing PDF content', unit="its"):
+            lv3_folder = list_directories(f"{lv1_folder}/{考試名稱}") #考試科目:list
+            for 考試科目 in lv3_folder:
+                lv3_path = f'{lv1_folder}/{考試名稱}/{考試科目}'
+                pdf_file = classify_files(lv3_path)
                 
-            q_parser = PDFQuestionParser(試題_path)
-            q_parser.integrate_answers(ans)
-            考卷內容 = q_parser.get_questions()
-            
-            考試資料 = {
-                "考試年份":考試年份,
-                "考試名稱":考試名稱,
-                "考試科目":考試科目,
-                "考卷內容":考卷內容
-            }
-            
-            data.append(考試資料)
+                試題_path = f'{lv3_path}/{pdf_file["試題"][0]}'
+                答案_path = f'{lv3_path}/{pdf_file["答案"][0]}'
+                # 檢查是否有更正答案，有則使用"更正答案"，反之則用"答案"
+                更正答案_path = [""]
+                if pdf_file["更正答案"]!=[]:
+                    更正答案_path = f'{lv3_path}/{pdf_file["更正答案"][0]}'
+                    ans = PDFAnswerExtractor(更正答案_path).get_results()
+                else: ans = PDFAnswerExtractor(答案_path).get_results()
+                    
+                q_parser = PDFQuestionParser(試題_path)
+                q_parser.integrate_answers(ans)
+                考卷內容 = q_parser.get_questions()
+                
+                考試資料 = {
+                    "考試年份":考試年份,
+                    "考試名稱":考試名稱,
+                    "考試科目":考試科目,
+                    "考卷內容":考卷內容
+                }
+                
+                data.append(考試資料)
 
-    # 存為json
-    with open(f'{考試年份}_考古題.json', 'w', encoding='utf-8') as json_file:
-        json.dump(data, json_file, ensure_ascii=False, indent=4) 
+        # 存為json
+        with open(f'{考試年份}_考古題.json', 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4) 
